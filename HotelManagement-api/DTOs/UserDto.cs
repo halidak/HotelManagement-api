cac9ebb48972adcabd2986317198519f019603ec
basilamed <101529372+basilamed@users.noreply.github.com>
2023-06-23 16:38:04 +0200

    kontroleri

--- HotelManagement-api/DTOs/UserDto.cs
+++ HotelManagement-api/DTOs/UserDto.cs
@@ -4,9 +4,9 @@ namespace HotelManagement_api.DTOs
 {
     public class UserDto
     {
-        [Required]
         public string FirstName { get; set; }
-        [Required]
         public string LastName { get; set; }
+        public string Image { get; set; }
+
     }
 }
